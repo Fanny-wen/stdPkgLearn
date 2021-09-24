9f300b4bd0e3b8cbb8c051b15306e87d94b6a10c
文周繁 <van203x@foxmail.com>
2021-09-24 18:03:19 +0800

    2021/9/24

---
+++ os_demo/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	//osOpenFileDemo()
+	//osReadDemo()
+	osWriteDemo()
+}
+
+func osOpenFileDemo() {
+	//fileObj, err := os.OpenFile("./hello.txt", os.O_CREATE|os.O_RDWR, 0666)
+	// openFile 是一个更一般性的文件打开函数，大多数调用者都应用 Open 或 Create 代替本函数。
+
+	//    O_RDONLY int = syscall.O_RDONLY // 只读模式打开文件
+	//    O_WRONLY int = syscall.O_WRONLY // 只写模式打开文件
+	//    O_RDWR   int = syscall.O_RDWR   // 读写模式打开文件
+	//    O_APPEND int = syscall.O_APPEND // 写操作时将数据附加到文件尾部
+	//    O_CREATE int = syscall.O_CREAT  // 如果不存在将创建一个新文件
+	//    O_EXCL   int = syscall.O_EXCL   // 和 O_CREATE 配合使用，文件必须不存在
+	//    O_SYNC   int = syscall.O_SYNC   // 打开文件用于同步 I/O
+	//    O_TRUNC  int = syscall.O_TRUNC  // 如果可能，打开时清空文件
+	fileObj, err := os.Create("./hello.txt")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stdout, "open file faild, err: %v\n", err)
+	}
+	defer fileObj.Close()
+	fmt.Println(fileObj)
+}
+
+/*
+ReadAt 从指定的位置（相对于文件开始位置）读取长度为 len(b) 个字节数据并写入 b。
+它返回读取的字节数和可能遇到的任何错误。当 n<len(b) 时，本方法总是会返回错误；如果是因为到达文件结尾，返回值 err 会是 io.EOF。
+它对应的系统调用是 pread。
+
+Read 和 ReadAt 的区别：前者从文件当前偏移量处读，且会改变文件当前的偏移量；
+而后者从 off 指定的位置开始读，且不会改变文件当前偏移量。
+ */
+func osReadDemo() {
+	b := make([]byte, 128)
+	fileObj, err := os.Open("./hello.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "open file faild, err: %v\n", err)
+	}
+	defer fileObj.Close()
+	for {
+		n, err := fileObj.Read(b)
+		if err == io.EOF {
+			fmt.Println("read fileObj end")
+			//return
+			break
+		}
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stdout, "read fileObj faild, err: %v\n", err)
+		}
+		// 获取文件对象当前偏移量
+		ret, err := fileObj.Seek(0, os.SEEK_CUR)
+		fmt.Println(ret)
+		fmt.Printf("read fileObj %d byte\n", n)
+		fmt.Println(string(b))
+	}
+
+	n, err := fileObj.ReadAt(b, 3)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "ReadAt file faild, err: %v\n", err)
+	}
+	fmt.Printf("file readAt method read %d byte\n", n)
+	fmt.Println(b[:n])
+	fmt.Println(string(b[:n]))
+	ret, err := fileObj.Seek(0, os.SEEK_CUR)
+	fmt.Println(ret)
+}
+
+
+func osWriteDemo(){
+	b := make([]byte, 128)
+	fileObj1, err := os.Open("./hello.txt")
+	n, err := fileObj1.Read(b)
+	if err == io.EOF {
+		fmt.Println("read fileObj end")
+		return
+	}
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stdout, "read fileObj faild, err: %v\n", err)
+		return
+	}
+	defer fileObj1.Close()
+
+	fileObj2, err := os.Create("./hello_2.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "create file faild, err: %v\n", err)
+		return
+	}
+	defer fileObj2.Close()
+	n ,err = fileObj2.Write(b)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "write file faild, err: %v\n", err)
+		return
+	}
+	fmt.Printf("write file success, write %d byte\n", n)
+
+	n, err = fileObj2.WriteAt([]byte("\nhello world"), 128)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "writeAt faild, err: %v\n", err)
+		return
+	}
+	fmt.Printf("writeAt success, write %d byte\n", n)
+}
