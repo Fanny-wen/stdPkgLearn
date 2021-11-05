1b9449df2edd22454231c0492c4645b4f620aa2f
文周繁 <van203x@foxmail.com>
2021-11-05 17:11:23 +0800

    2021/11/5 gin upload

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -19,9 +19,14 @@ func init() {
 	ApiParameter(r)
 	UrlParameter(r)
 	FormParameter(r)
+	UploadDemo(r)
+	UploadMultipleDemo(r)
 }
 
 func main() {
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
 	_ = r.Run("localhost:8000")
@@ -41,8 +46,8 @@ func ApiParameter(r *gin.Engine) {
 
 // UrlParameter URL参数
 func UrlParameter(r *gin.Engine) {
-	//URL参数可以通过DefaultQuery()或Query()方法获取
-	//DefaultQuery()若参数不村则，返回默认值，Query()若不存在，返回空串
+	//URL参数可以通过DefaultQuery()或Query(), QueryArray(), QueryMap()方法获取
+	//DefaultQuery()若参数不存在，返回默认值，Query()若不存在，返回空串
 	r.GET("/welcome", func(c *gin.Context) {
 		fmt.Printf("%T, %#v\n", c.Request.URL, c.Request.URL)
 		fmt.Printf("%v, %[1]T\n", c.Request.URL.Query())
@@ -63,5 +68,54 @@ func FormParameter(r *gin.Engine) {
 		parent := c.PostFormMap("parent")
 		fmt.Printf("name: %v, pwd: %s, hobby: %v, parent: %v, type: %s\n", name, pwd, hobby, parent, types)
 		c.JSON(http.StatusCreated, gin.H{"name": name, "pwd": pwd, "hobby": hobby, "parent": parent})
+	})
+}
+
+// UploadDemo 上传文件
+func UploadDemo(r *gin.Engine) {
+	r.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		fmt.Printf("filename: %s\n", file.Filename)
+		err := c.SaveUploadedFile(file, file.Filename)
+		if err != nil {
+			fmt.Printf("save uploaded file failed! err: %v\n", err)
+			panic("save uploaded file failed!")
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"file": file.Filename,
+			"size": file.Size,
+		})
+	})
+}
+
+// UploadMultipleDemo 多文件
+func UploadMultipleDemo(r *gin.Engine) {
+	r.POST("/upload_multiple", func(c *gin.Context) {
+		var data []interface{}
+		form, err := c.MultipartForm()
+		if err != nil {
+			fmt.Printf("multipartForm failed! err: %v\n", err)
+			panic("multipartForm failed!")
+		}
+		files := form.File["file"]
+		value := form.Value
+		fmt.Printf("form.File's value: %v\n", value)
+		for _, file := range files {
+			fmt.Printf("filename: %s\n", file.Filename)
+			err := c.SaveUploadedFile(file, file.Filename)
+			if err != nil {
+				fmt.Printf("save uploaded file failed! err: %v\n", err)
+				panic("save uploaded file failed!")
+			}
+			info := map[string]interface{}{
+				"filename": file.Filename,
+				"size": file.Size,
+			}
+			data = append(data, info)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
 	})
 }
