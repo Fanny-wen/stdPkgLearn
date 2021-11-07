22686c2ce51edeb6a8fb0446604cb4888e3f3049
wenzhoufan <van203x@foxmail.com>
2021-11-07 22:46:25 +0800

    2021/11/7 gin

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -16,11 +16,18 @@ func init() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello World!")
 	})
+	PageNotFindDemo(r)
+	NORouterDemo(r)
 	ApiParameter(r)
 	UrlParameter(r)
 	FormParameter(r)
-	UploadDemo(r)
-	UploadMultipleDemo(r)
+	RedirectDemo(r)
+	// Grouping routes
+	upload := r.Group("/upload")
+	{
+		upload.POST("", UploadDemo)
+		upload.POST("/multiple", UploadMultipleDemo)
+	}
 }
 
 func main() {
@@ -30,6 +37,23 @@ func main() {
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
 	_ = r.Run("localhost:8000")
+}
+
+// NORouterDemo 重新向到页面
+func NORouterDemo(r *gin.Engine) {
+	r.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/404")
+	})
+}
+
+// PageNotFindDemo  404页面
+func PageNotFindDemo(r *gin.Engine) {
+	r.LoadHTMLGlob("./*")
+	r.GET("/404", func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.html", gin.H{
+			"title": "404",
+		})
+	})
 }
 
 // ApiParameter API参数
@@ -72,10 +96,10 @@ func FormParameter(r *gin.Engine) {
 }
 
 // UploadDemo 上传文件
-func UploadDemo(r *gin.Engine) {
-	r.POST("/upload", func(c *gin.Context) {
-		// single file
-		file, _ := c.FormFile("file")
+func UploadDemo(c *gin.Context) {
+	// single file
+	file, _ := c.FormFile("file")
+	if file != nil {
 		fmt.Printf("filename: %s\n", file.Filename)
 		err := c.SaveUploadedFile(file, file.Filename)
 		if err != nil {
@@ -83,39 +107,60 @@ func UploadDemo(r *gin.Engine) {
 			panic("save uploaded file failed!")
 		}
 		c.JSON(http.StatusOK, gin.H{
-			"file": file.Filename,
-			"size": file.Size,
+			"file":    file.Filename,
+			"size":    file.Size,
+			"message": "success",
+			"status":  200,
 		})
-	})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "error",
+			"status":  400,
+		})
+	}
 }
 
 // UploadMultipleDemo 多文件
-func UploadMultipleDemo(r *gin.Engine) {
-	r.POST("/upload_multiple", func(c *gin.Context) {
-		var data []interface{}
-		form, err := c.MultipartForm()
+func UploadMultipleDemo(c *gin.Context) {
+	defer func() {
+		if err := recover(); err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"message": err,
+				"status":  400,
+			})
+		}
+	}()
+	var data []interface{}
+	form, err := c.MultipartForm()
+	if form == nil && err != nil {
+		panic(err)
+	}
+	files := form.File["file"]
+	value := form.Value // Value 属性保存除 文件外的其他body数据
+	fmt.Printf("form's value: %v\n", value)
+
+	for _, file := range files {
+		fmt.Printf("filename: %s\n", file.Filename)
+		err := c.SaveUploadedFile(file, file.Filename)
 		if err != nil {
-			fmt.Printf("multipartForm failed! err: %v\n", err)
-			panic("multipartForm failed!")
+			fmt.Printf("save uploaded file failed! err: %v\n", err)
+			panic("save uploaded file failed!")
 		}
-		files := form.File["file"]
-		value := form.Value
-		fmt.Printf("form.File's value: %v\n", value)
-		for _, file := range files {
-			fmt.Printf("filename: %s\n", file.Filename)
-			err := c.SaveUploadedFile(file, file.Filename)
-			if err != nil {
-				fmt.Printf("save uploaded file failed! err: %v\n", err)
-				panic("save uploaded file failed!")
-			}
-			info := map[string]interface{}{
-				"filename": file.Filename,
-				"size": file.Size,
-			}
-			data = append(data, info)
+		info := map[string]interface{}{
+			"filename": file.Filename,
+			"size":     file.Size,
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"data": data,
-		})
+		data = append(data, info)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": data,
+	})
+}
+
+// RedirectDemo 重定向
+func RedirectDemo(r *gin.Engine) {
+	r.GET("/redirect", func(c *gin.Context) {
+		//c.Redirect(http.StatusMovedPermanently, "http://www.baidu.com/")
+		c.Redirect(http.StatusMovedPermanently, "/404")
 	})
 }
